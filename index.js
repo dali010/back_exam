d75fd6559f31afd2d49cc180c03af659139053d9
dali010 <mohamedali.hmida@isimg.tn>
2024-11-05 09:12:20 +0100

    update text

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
     app.listen(PORT, () =>
-      console.log(`Server Running on Port hello !! : http://localhost:${PORT}`)
+      console.log(`Server Running on Port  : http://localhost:${PORT}`)
     )
   )
   .catch((error) => console.log(`${error} did not connect`));
